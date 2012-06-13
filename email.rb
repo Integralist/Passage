0f292f95ed99368869c9952124c1e8a784497059
Mark McDonnell <mark.mcdx@gmail.com>
2012-06-13 08:42:51 +0100

    Email

--- email.rb
+++ email.rb
@@ -1,5 +1,12 @@
 require 'net/smtp'
 
+=begin
+	http://apidock.com/ruby/v1_9_3_125/Net/SMTP/start/class
+	https://devcenter.heroku.com/articles/config-vars
+	heroku config:add GM_USR=xxxx
+	heroku config:add GM_PSW=xxxx
+=end
+
 class Email
 	def initialize (to, from, subject, body)
 		@to = to
@@ -14,11 +21,14 @@ class Email
 		    Subject: #{@subject}
 		    #{@body}
 MESSAGE_CONTENT
+
+		@smtp = Net::SMTP.new('smtp.gmail.com', 587)
 	end
 	
 	def send_email
-		Net::SMTP.start('localhost') do |smtp|
-			smtp.send_message(@message, @from, @to)
+		@smtp.enable_starttls
+		@smtp.start('furious-wind-9309.herokuapp.com', ENV['GM_USR'], ENV['GM_PSW'], :login) do |smtp|
+			@smtp.send_message(@message, @from, @to)
 		end
 	end
 end
